761140284d4af5c428564b4448e8cb59f8fda067
Amir Sejdinovic <amir24V@yahoo.com>
2019-09-13 15:03:28 +0200

    super

---
+++ proces.php
@@ -0,0 +1,24 @@
+<?php
+if(isset($_POST['submit']))
+
+ $title =  $_POST['title'];
+ $firstName =  $_POST['firstName'];
+ $email = $_POST['email'];
+ $company = $_POST['company'];
+ $street = $_POST['street'];
+ $zip = $_POST['zip'];
+ $city = $_POST['city'];
+ $country = $_POST['country'];
+ $material = $_POST['material'];
+ $privacyStatement = $_POST['privacyStatement'];
+ $hotelYes = $_POST['hotelYes'];
+ $hotelNo = $_POST['hotelNo'];
+ $specialDemands = $_POST['specialDemands'];
+
+ echo $title;
+ 
+
+
+
+
+?>
