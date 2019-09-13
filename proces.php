461221a909cdf964711dc015b02f328c8651eb8b
Amir Sejdinovic <amir24V@yahoo.com>
2019-09-13 15:07:39 +0200

    Update proces.php

--- proces.php
+++ proces.php
@@ -1,6 +1,13 @@
 <?php
 if(isset($_POST['submit']))
 
+ $formok = true;
+ $errors = array();
+
+ $ipadress = $_SERVER['REMOTE_ADDR'];
+ $date = date('d/m/Y');
+ $time = date('H:i:s');
+
  $title =  $_POST['title'];
  $firstName =  $_POST['firstName'];
  $email = $_POST['email'];
@@ -9,15 +16,48 @@ if(isset($_POST['submit']))
  $zip = $_POST['zip'];
  $city = $_POST['city'];
  $country = $_POST['country'];
- $material = $_POST['material'];
- $privacyStatement = $_POST['privacyStatement'];
- $hotelYes = $_POST['hotelYes'];
- $hotelNo = $_POST['hotelNo'];
- $specialDemands = $_POST['specialDemands'];
-
- echo $title;
+ //$material = $_POST['material'];
  
 
+ //$hotelNo = $_POST['hotelNo'];
+ $specialDemands = $_POST['specialDemands'];
+
+
+ if(isset($_POST['material'])){
+  $material = $_POST['material'];
+  $material = "Yes";
+}else{
+  $material = "no";
+}
+
+if(isset($_POST['privacyStatement'])){
+  $privacyStatement = $_POST['privacyStatement'];
+  $privacyStatement = "Yes";
+}else{
+  $privacyStatement = "No";
+}
+
+if(isset($_POST['hotelYes'])){
+  $hotelYes = $_POST['hotelYes'];
+}else{
+  $hotelYes = "";
+}
+
+
+ //validacija forme
+ if(empty($title && $firstName && $company && $street && $zip && $city && $country)){
+   $formok = false;
+   $errors[] = "You have not fill require fields";
+ }
+
+ if($formok){
+   
+ echo $title;
+ echo $material;
+ echo  $specialDemands;
+ }
+
+ 
 
 
 
